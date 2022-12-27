2023968a3a5ea03b71435719c708584de96ec7d1
marugy <gyulife7301@naver.com>
2022-12-27 11:55:12 +0900

    Object

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-const daysOfWeek = ["mon", "tue", "wed", "fri", "sat"];
+const playerName = "nico";
+const playerPoints = 121212;
+const playerHandsome = false;
+const playerFat = "little bit";
 
-console.log(daysOfWeek);
+const player = {
+  name: "nico",
+  points: 10,
+  handsome: false,
+  fat: true,
+};
+
+console.log(player);
+console.log(player.name);
+
+//내부 값도 변경 가능
+player.name = "nicolas";
+console.log(player["name"]);
+
+player.lastName = "potato";
+console.log(player);
