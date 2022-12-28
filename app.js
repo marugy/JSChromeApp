08910cd6c80bcba635488ea18c31aa0d64a6ae3f
marugy <gyulife7301@naver.com>
2022-12-28 19:07:52 +0900

    Function

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
-const playerName = "nico";
-const playerPoints = 121212;
-const playerHandsome = false;
-const playerFat = "little bit";
+function sayHello() {
+  console.log("hello!!!1");
+}
 
-const player = {
-  name: "nico",
-  points: 10,
-  handsome: false,
-  fat: true,
-};
+function sayHelloName(name) {
+  console.log("hello my name is " + name);
+}
 
-console.log(player);
-console.log(player.name);
-
-//내부 값도 변경 가능
-player.name = "nicolas";
-console.log(player["name"]);
-
-player.lastName = "potato";
-console.log(player);
+sayHello();
+sayHelloName("nico");
